1d44a11cf5b98405222a125714fab3262e5351f6
Aamir <buchh1aamir@gmail.com>
2021-11-21 19:47:55 +0530

    some minor changes in c-programs

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,7 @@
 
+   int i = 0, j =1;
+   printf("The logical operator returned for and is %d\n",i&&j);
+   printf("The logical operator returned for or is %d\n",i||j);
+   printf("The logical operator returned for not is %d\n",!j);
+   printf("The logical operator returned for not is %d\n",!i)
+   
