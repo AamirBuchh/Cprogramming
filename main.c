b56f6d488f291ed5ae5b12ca1f7aef6395180513
Aamir <buchh1aamir@gmail.com>
2021-11-22 23:25:35 +0530

    Worked on more of c programming

--- main.c
+++ main.c
@@ -1,51 +1,51 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <conio.h>
 
 int main()
 {
-    //single line comment ,compiler will ignore this 
-    
+    //single line comment ,compiler will ignore this
+
     /*
     multiline comment
     */
 
-   //
+    //single line comment
 
-   //int, lfloat, char
+    //int, lfloat, char
 
-   /* nt a = 7;//2 to 4 bytes
-   unsigned int e = 7;
-   long int f = 8.1;
-   short int g = 'l';
+    //    nt a = 7;//2 to 4 bytes
+    //    unsigned int e = 7;
+    //    long int f = 8.1;
+    //    short int g = 'l';
 
-   float b = 8.1;//4 bytes - 6 decimal precison 
-   double myfloat1 = 7.45;//15 decimal places 
-   long double myfloat = 7.65;//19 decimal places
-   
-   char c = 'b';//1 byte
+    //    float b = 8.1;//4 bytes - 6 decimal precison
+    //    double myfloat1 = 7.45;//15 decimal places
+    //    long double myfloat = 7.65;//19 decimal places
 
-   printf("The size taken by int is %d\n",sizeof(int));
-   printf("The size taken by unsizned int is %d\n",sizeof(unsigned int));
-   printf("The size taken by long int is %d\n",sizeof(long int));
-   printf("The size taken by short int is %d\n",sizeof(short int));
-   
-   printf("The size taken by char is %c",sizeof(char));
-   printf("The size taken by unsigned char is %c",sizeof(unsigned char));
-   printf("The size taken by unsigned long char is %c",sizeof(char));
-   
+    //    char c = 'b';//1 byte
 
-   printf("hello %d\n",a);
-   printf("hello %f\n",b);
-   printf("hello %c\n",c); */
+    //    printf("The size taken by int is %d\n",sizeof(int));
+    //    printf("The size taken by unsizned int is %d\n",sizeof(unsigned int));
+    //    printf("The size taken by long int is %d\n",sizeof(long int));
+    //    printf("The size taken by short int is %d\n",sizeof(short int));
 
-   /* printf("******Rules for creating variables******");
-   int aamirName; // This is variable declaration
-   aamir = 101; // This is variable initialization 
-   char five = "5" ;// initialization + declaration
+    //    printf("The size taken by char is %c",sizeof(char));
+    //    printf("The size taken by unsigned char is %c",sizeof(unsigned char));
+    //    printf("The size taken by unsigned long char is %c",sizeof(char));
 
-   const int i = 9; // doesn't change through out program
-   //i = 10; -error
-   aamirName = 89; */
-   
+    //    printf("hello %d\n",a);
+    //    printf("hello %f\n",b);
+    //    printf("hello %c\n",c);
+
+    //    printf("******Rules for creating variables******");
+    //    int aamirName; // This is variable declaration
+    //    aamir = 101; // This is variable initialization
+    //    char five = "5" ;// initialization + declaration
+
+    //    const int i = 9; // doesn't change through out program
+    //    //i = 10; -error
+    //    aamirName = 89;
+
     // printf("******Types of operators in c******");
     /* 
     Arithmetic operators
@@ -55,68 +55,87 @@ int main()
     Assignment operators
     Misc operators
     */
-/* 
-   //Arithmetic operators
-   int a = 6, b = 7, c = 8;
-   printf("The sum of %d and %d is %d\n",a,b,a+b);
-   printf("The difference of %d and %d is %d\n",a,b,a-b);
-   printf("The multiplication of %d and %d is %d\n",a,b,a*b);
-   printf("The division of %d and %d is %d\n",a,b,a/b);
-   printf("The remainder of division of %d and %d is %d\n",a,b,a%b);
-   printf("Post Increment of a : %d\n",a++);
-   printf("Post Decrement of a : %d\n",a--);
-   printf("Pre Increment of a : %d\n",++a);
-   printf("Pre Decrement of a : %d\n",--a);
-    */
 
-   //Relational operators
-   /* 
-   int asif = 91, aamir=98;
-   printf("%d\n",asif==aamir);
-   printf("%d\n",asif!=aamir);
-   printf("%d\n",asif>=aamir);
-   printf("%d\n",asif<=aamir);
-    */
-   //abc
-   //Logical operators
-   
-   /* int i = 0, j =1;
-   printf("The logical operator returned for and is %d\n",i&&j);
-   printf("The logical operator returned for or is %d\n",i||j);
-   printf("The logical operator returned for not is %d\n",!j);
-   printf("The logical operator returned for not is %d\n",!i)
-    */
+    //Arithmetic operators
+    //    int a = 6, b = 7, c = 8;
+    //    printf("The sum of %d and %d is %d\n",a,b,a+b);
+    //    printf("The difference of %d and %d is %d\n",a,b,a-b);
+    //    printf("The multiplication of %d and %d is %d\n",a,b,a*b);
+    //    printf("The division of %d and %d is %d\n",a,b,a/b);
+    //    printf("The remainder of division of %d and %d is %d\n",a,b,a%b);
+    //    printf("Post Increment of a : %d\n",a++);
+    //    printf("Post Decrement of a : %d\n",a--);
+    //    printf("Pre Increment of a : %d\n",++a);
+    //    printf("Pre Decrement of a : %d\n",--a);
 
-   //Bitwise operators
-   
-   /* 
-  int A = 60 , B = 14;
-//    A = 00111100;
-//    B = 00001101;
-   printf("Bitwise and operation: %d\n", A&B);
-   printf("Bitwise or operation: %d\n", A|B);
-   printf("Bitwise xor operation: %d\n", A^B);
-   printf("Bitwise ones compliment operator: %d\n", ~B);
-   printf("Bitwise left shift operation: %d\n", A<<B);
-   printf("Bitwise right shift operation: %d\n",A>>B);
+    //Relational operators
 
-    */
-    
-    //Assignment operator
+    //    int asif = 91, aamir=98;
+    //    printf("%d\n",asif==aamir);
+    //    printf("%d\n",asif!=aamir);
+    //    printf("%d\n",asif>=aamir);
+    //    printf("%d\n",asif<=aamir);
 
-    //=, +=, -=, *=, %=, etc
+    //abc
+    //Logical operators
 
-    /* int ha = 9;
-    ha += 9;
-    printf("ha is %d\n",ha);
-    ha *= 9;
-    printf("ha is %d\n",ha);
-    ha %= 9;
-    printf("ha is %d\n",ha); */
+    //    int i = 0, j =1;
+    //    printf("The logical operator returned for and is %d\n",i&&j);
+    //    printf("The logical operator returned for or is %d\n",i||j);
+    //    printf("The logical operator returned for not is %d\n",!j);
+    //    printf("The logical operator returned for not is %d\n",!i)
 
+    //Bitwise operators
+
+    //   int A = 60 , B = 14;
+    // //    A = 00111100;
+    // //    B = 00001101;
+    //    printf("Bitwise and operation: %d\n", A&B);
+    //    printf("Bitwise or operation: %d\n", A|B);
+    //    printf("Bitwise xor operation: %d\n", A^B);
+    //    printf("Bitwise ones compliment operator: %d\n", ~B);
+    //    printf("Bitwise left shift operation: %d\n", A<<B);
+    //    printf("Bitwise right shift operation: %d\n",A>>B);
+
+    // //Assignment operator
+
+    // //=, +=, -=, *=, %=, etc
+
+    // int ha = 9;
+    // ha += 9;
+    // printf("ha is %d\n",ha);
+    // ha *= 9;
+    // printf("ha is %d\n",ha);
+    // ha %= 9;
+    // printf("ha is %d\n",ha);
 
     //Misc operators: &(address), *(value at address), ?:(conditional statement)
 
+    // printf("\n\n*****Taking input from c*****\n");
+    // int aamirInput;
+    // printf("\nPlease enter the value of aamirInput:");
+    // scanf("%d",&aamirInput);
+    // // printf("\nYou entered input as : %d \n\n",(float)aamirInput);
+    // printf("\nThe value of int converted into float : %0.3f\n",(float)aamirInput);
 
-   return 0;
+    // int num1, num2;
+    // printf("num1:");scanf("%d",&num1);
+    // printf("num2:");scanf("%d",&num2);
+    // printf("\n num1/num2 : %0.2f \n",(float)num1/num2);
+
+    // int alpha;
+    // printf('\nEnter to alphabets to print from A to D in upper case only : ');
+    // scanf("%d", &alpha);
+    // printf(alpha);
+    // switch (alpha)
+    // {
+    // case 1:
+    //     printf("\nSuccess\n");
+    //     break;
+
+    // default:
+    //     break;
+    // }
+
+    return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,7 +1,5 @@
-
-   int i = 0, j =1;
-   printf("The logical operator returned for and is %d\n",i&&j);
-   printf("The logical operator returned for or is %d\n",i||j);
-   printf("The logical operator returned for not is %d\n",!j);
-   printf("The logical operator returned for not is %d\n",!i)
-   
+for (int i = 0; i < 10; i++)
+    // {
+    //     printf("Enter the values for the index %d \n",i);
+    //     scanf("%d",&arr[i]);
+    // }
